4b3613633534d856d4bd35477872a30a1ab15b80
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2025-03-06 14:35:29 +0700

    stock history + store checkout

--- src/app/guards/store.guard.ts
+++ src/app/guards/store.guard.ts
@@ -5,6 +5,8 @@ import { inject } from '@angular/core';
 import { UserService } from '../services/user/user.service';
 import { UserResponse} from '../dto/Response/user/user.response';
 import { TokenService } from '../services/token/token.service';
+import {StaffService} from '../services/staff/staff.service';
+import {catchError, map, Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,32 +16,52 @@ export class StoreGuard  {
   constructor(
     private tokenService: TokenService,
     private router: Router,
-    private userService:UserService
+    private userService:UserService,
+    private staffService:StaffService
   ) {}
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
     const isTokenExpired = this.tokenService.isTokenExpired();
     const isUserIdValid = this.tokenService.getUserId() > 0;
-    this.user_info = this.userService.getUserInfo()
 
-    const userRole = this.user_info?.roles;
-    const isStaff = userRole == 'ROLE_STAFF';
-    const isStoreManager = userRole == 'ROLE_STORE_MANAGER';
 
-    if (!isTokenExpired && isUserIdValid && (isStaff || isStoreManager)) {
-      return true;
-    } else {
-      this.router.navigate(['staff/0/login'], {
+    if (isTokenExpired || !isUserIdValid) {
+      return of(this.router.createUrlTree(['staff/0/login'], {
         queryParams: { error: 'YOU DONT HAVE PERMISSION' },
-      });
-      return false;
+      }));
     }
-  }
-}
 
+    const userId = this.tokenService.getUserId();
+    const storeId = Number(next.paramMap.get('storeId'));
+
+    this.user_info = this.userService.getUserInfo();
+    const userRole = this.user_info?.roles;
+    const isStaff = userRole?.includes('ROLE_STAFF') ?? false;
+    const isStoreManager = userRole?.includes('ROLE_STORE_MANAGER') ?? false;
+
+
+
+    return this.staffService.checkUserInStore(userId, storeId).pipe(
+      map(isInStore => {
+        if (storeId === 0) {
+          return true;
+        }
+
+        if (isInStore && (isStaff || isStoreManager)) {
+          return true;
+        } else {
+          return this.router.createUrlTree(['staff/0/login'], {
+            queryParams: { error: 'YOU DONT HAVE PERMISSION' },
+          });
+        }
+      })
+    );
+  }
+
+}
 export const StoreGuardFn: CanActivateFn = (
   next: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
-): boolean => {
+): Observable<boolean | UrlTree> => {
   return inject(StoreGuard).canActivate(next, state);
 };
