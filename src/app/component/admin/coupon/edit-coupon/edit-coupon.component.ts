2a6e04861130beb68166dc70603268e08fbf72a6
ducminhhh <doducminh141000@gmail.com>
2025-03-12 14:37:21 +0700

    update coupon page admin

--- src/app/component/admin/coupon/edit-coupon/edit-coupon.component.ts
+++ src/app/component/admin/coupon/edit-coupon/edit-coupon.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatTable} from '@angular/material/table';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-edit-coupon',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTable,
+    MatIcon
+  ],
   templateUrl: './edit-coupon.component.html',
   styleUrl: './edit-coupon.component.scss'
 })
