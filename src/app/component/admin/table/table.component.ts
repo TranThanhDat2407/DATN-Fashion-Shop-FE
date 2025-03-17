a568a9b55a7fd62d077ada197eaab2b5fd0400df
Tai <bvt.vantai@gmail.com>
2025-03-17 18:34:51 +0700

    update hmtl new

--- src/app/component/admin/table/table.component.ts
+++ src/app/component/admin/table/table.component.ts
@@ -144,6 +144,44 @@ export class TableComponent implements OnInit {
 
 
 
+  setPage(page: number) {
+    this.currentPage = page;
+    this.page =page
+    this.pageChanged.emit(page);  // Phát sự kiện
+  }
+
+  // Lấy dữ liệu phân trang
+  get paginatedData() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    return this.dataPage.content.slice(start, end); // Lấy dữ liệu từ content
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.currentPage = this.page;
+      this.pageChanged.emit(this.page);
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.currentPage = this.page;
+      this.pageChanged.emit(this.page);
+    }
+  }
+
+
+
+  get totalPages() {
+    return this.dataPage?.totalPages ? Math.ceil(this.dataPage.totalPages) : 0;
+  }
+
+  get totalElements() {
+    return this.dataPage.totalElements; // Lấy tổng số phần tử từ dữ liệu
+  }
 
 }
 
